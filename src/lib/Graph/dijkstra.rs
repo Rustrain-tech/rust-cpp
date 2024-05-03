7aa5853974519e754941b68e8452014598d5fa22
rusty <codingtrain9@gmail.com>
2024-05-03 12:35:56 +0900

    dijkstra

---
+++ src/lib/Graph/dijkstra.rs
@@ -0,0 +1,24 @@
+use crate::lib::Graph::graph::Edge;
+use crate::lib::Graph::graph::Graph;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+fn dijkstra(graph: &Graph, start: usize) -> Vec<i64> {
+    let mut dist = vec![1 << 60; graph.edges.len()];
+    let mut heap = BinaryHeap::new();
+    dist[start] = 0;
+    heap.push((Reverse(0), start));
+    while let Some((Reverse(cost), node)) = heap.pop() {
+        if dist[node] < cost {
+            continue;
+        }
+        for &Edge { from, to, cost } in &graph.edges[node] {
+            let new_cost = cost + dist[node];
+            if new_cost < dist[to] {
+                dist[to] = new_cost;
+                heap.push((Reverse(new_cost), to));
+            }
+        }
+    }
+    dist
+}
